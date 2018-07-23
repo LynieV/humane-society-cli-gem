f789161588a25879dbdbb889c6fdc15fca6bef45
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-07-23 01:06:36 +0000

    add notes to cli.rb, updated cli filename to cli.rb

---
+++ HumaneSociety/lib/HumaneSociety/cli.rb
@@ -0,0 +1,29 @@
+class CLI
+
+  def start
+    puts "welcome to Hornell Humane Society"
+    puts ""
+  end
+
+end
+
+#User will see a greeting and be asked to enter a number
+#corresponding to a prompt:
+#1 Exit
+#2 Financial
+#3 Supply List
+
+#  list.each.with_index(1) do |list, index|
+#    puts "#{index}. #{list.title}"
+#  end
+
+#when user chooses supply list, they will see a list
+#of headings for items needed
+
+#from this list, the user will be asked to enter the number
+#for the heading and will see the list of items from that heading
+#or go back to the main menu
+
+#there is a supply class to represent each category
+#of supplies. A supply scraper to scrape the site and Humane Society
+#CLI to interact with the user
