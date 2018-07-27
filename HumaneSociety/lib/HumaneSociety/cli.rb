58590b218ac2546d18eef546493faf2326b04d0b
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-07-27 01:42:41 +0000

    change supply.rb to supplies.rb

--- HumaneSociety/lib/HumaneSociety/cli.rb
+++ HumaneSociety/lib/HumaneSociety/cli.rb
@@ -6,10 +6,11 @@ class HumaneSociety::CLI
     puts "Welcome to Hornell Humane Society donations."
     puts ""
     list_options
-    menu
-    goodbye
+    main_menu
+    #goodbye
 
     HumaneSociety::Scraper.scrape_donations
+    #binding.pry
     #main_menu
   end
 
@@ -20,61 +21,82 @@ class HumaneSociety::CLI
     puts "Type exit to exit"
   end
 
-  def menu
+  def main_menu
     input = nil
-    while input != "exit"
-      puts ""
-      puts "Please enter the number for your choice or type exit:"
-      input = gets.strip.downcase
-      case input
-      when "1"
-        puts "Please send checks to:"
-        puts ""
-        puts "Hornell Humane Society"
-        puts "7649 Industrial Park Road"
-        puts "Hornell, NY 14843"
-      when "2"
-        puts "Supply Headings"
-      #when "list"
-        #list_options
-      else
-        "Please choose a valid option or type exit"
-      end
+     while input != "exit"
+       puts ""
+       puts "Please enter the number for your choice or type exit:"
+       input = gets.strip.downcase
+       #case input
+       #when "1"
+       if input == "1"
+         puts "Please send checks to:"
+         puts ""
+         puts "Hornell Humane Society"
+         puts "7649 Industrial Park Road"
+         puts "Hornell, NY 14843"
+       #when "2"
+       elsif input == "2"
+    puts "Here is a list of donation options!"
+
+    donations = HumaneSociety::Supplies.all
+
+    donations.each.with_index(1) do |index, donate|
+      puts "#{index}. #{donate.heading}"
     end
+    #binding.pry
+    puts ""
+    puts "Please select a donation type to view needed items:"
+
+    input = gets.strip
+    index = input.to_i - 1
+
+    supply = HumaneSociety::Supplies.all[index]
+
+    HumaneSociety::Scraper.scrape_donations_items(donation_type)
+
+    puts "Here are the items:"
+    puts ""
+    puts supply.list_items
+    #binding.pry
+    main_menu
+
   end
-
-  #def main_menu
-    #puts "Here is a list of donation options!"
-
-    #donations = HumaneSociety::Supplies.all
-
-    #donations.each.with_index(1) do |index, donate|
-      #puts "#{index}. #{donate.heading}"
-    #end
-
-    #puts ""
-    #puts "Please select a donation type to view needed items:"
-
-    #input = gets.strip
-    #index = input.to_i - 1
-
-    #supply = HumaneSociety::Supplies.all[index]
-
-    #HumaneSociety::Scraper.scrape_donations_items(donation_type)
-
-    #puts "Here are the items:"
-    #puts ""
-    #puts supply.list_items
-
-    #main_menu
-
-  #end
 
   def goodbye
     puts "Thank you!"
   end
 
 end
+
+#def menu
+  #input = nil
+#   while input != "exit"
+#     puts ""
+#     puts "Please enter the number for your choice or type exit:"
+#     input = gets.strip.downcase
+#     #case input
+#     #when "1"
+#     if input == "1"
+#       puts "Please send checks to:"
+#       puts ""
+#       puts "Hornell Humane Society"
+#       puts "7649 Industrial Park Road"
+#       puts "Hornell, NY 14843"
+#     #when "2"
+#     elsif input == "2"
+#
+#       puts "Supplies needed:"
+#       supplies = HumaneSociety::Supplies.all
+#
+#     #when "list"
+#       #list_options
+#     else
+#       "Please choose a valid option or type exit"
+#     end
+#   end
+# end
+
 
 #User will see a greeting and be asked to enter a number
 #corresponding to a prompt:
